c7bc2640d1c1b38a9b2ec0586976da1b34f508fc
arachoxs <j.agudelo9@utp.edu.co>
2026-03-12 15:09:37 -0500

    feauture/se actualiza la version mobile de la vista

--- biblioteca-alejandria/app/registro/RegistroForm.tsx
+++ biblioteca-alejandria/app/registro/RegistroForm.tsx
@@ -163,8 +163,8 @@ export default function RegistroForm() {
                     Datos personales
                 </legend>
 
-                <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
-                    <div className="col-span-1 sm:col-span-3 grid grid-cols-2 gap-5">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                    <div className="col-span-1 md:col-span-3 grid grid-cols-1 md:grid-cols-2 gap-5">
                         <Input
                         id="dni"
                         name="dni"
