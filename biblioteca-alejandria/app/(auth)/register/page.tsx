7266fa217561e411f18f675497565b57dd376c58
arachoxs <j.agudelo9@utp.edu.co>
2026-03-25 13:37:07 -0500

    fix:view-registro

--- biblioteca-alejandria/app/(auth)/register/page.tsx
+++ biblioteca-alejandria/app/(auth)/register/page.tsx
@@ -39,7 +39,7 @@ export default function Registro() {
                 </header>
 
                 {/* ── Formulario ── */}
-                <main className="flex-2 px-4 md:px-10 lg:px-30 bg-white/50 py-10 lg:h-full lg:overflow-hidden">
+                <main className="flex-2 px-4 md:px-10 lg:px-30 bg-white/50 py-10 lg:h-full lg:overflow-scroll flex items-center justify-center">
                     <RegistroForm />
                 </main>
 
