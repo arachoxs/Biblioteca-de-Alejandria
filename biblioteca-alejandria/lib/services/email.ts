ca3d4679d0cf3ead58896f6704806d6a3d2c7b8d
arachoxs <j.agudelo9@utp.edu.co>
2026-03-25 16:29:33 -0500

    fix:send-mail
    se implementa el envio de correos atravez de un acuenta de gmail

--- biblioteca-alejandria/lib/services/email.ts
+++ biblioteca-alejandria/lib/services/email.ts
@@ -1,19 +1,13 @@
 import nodemailer from 'nodemailer';
 import { MailSentResponse } from '../types/auth';
 
-// Configuration for Maileroo SMTP
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILEROO_SMTP_HOST || 'smtp.maileroo.com',
-  port: Number(process.env.MAILEROO_SMTP_PORT) || 465, // Use 465, 587, or 2525
-  secure: Number(process.env.MAILEROO_SMTP_PORT) === 465, // true for 465, false for other ports
+
+const transporterGmail = nodemailer.createTransport({
+  service: 'gmail',
   auth: {
-    user: process.env.MAILEROO_SMTP_USER,
-    pass: process.env.MAILEROO_SMTP_PASSWORD,
+    user: process.env.GMAIL_USER,
+    pass: process.env.CLAVE_APP_GMAIL,
   },
-  // Añade estas líneas para manejar problemas de conexión:
-  connectionTimeout: 10000, // 10 segundos
-  greetingTimeout: 5000,
-  socketTimeout: 10000
 });
 
 type SendEmailOptions = {
@@ -26,7 +20,7 @@ type SendEmailOptions = {
 
 export const sendEmail = async ({ to, subject, html, text, from }: SendEmailOptions): Promise<MailSentResponse> => {
   const mailOptions = {
-    from: from || process.env.MAILEROO_FROM_EMAIL || process.env.MAILEROO_SMTP_USER,
+    from: from || process.env.GMAIL_USER,
     to,
     subject,
     html,
@@ -34,9 +28,9 @@ export const sendEmail = async ({ to, subject, html, text, from }: SendEmailOpti
   };
 
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Message sent: %s', info.messageId);
-    return { success: true, message: info.messageId };
+    const info = await transporterGmail.sendMail(mailOptions);
+    console.log('Message sent: %s', info);
+    return { success: true, message: info.response };
   } catch (error) {
     console.error('Error sending email:', error);
     return { success: false, message: "Error al enviar el correo." };
