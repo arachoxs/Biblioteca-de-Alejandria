39da1b1db3bfb226f4e64f8f2705fe520553ec5c
arachoxs <j.agudelo9@utp.edu.co>
2026-03-25 11:58:00 -0500

    fix:overflow

--- biblioteca-alejandria/app/(auth)/register/page.tsx
+++ biblioteca-alejandria/app/(auth)/register/page.tsx
@@ -39,7 +39,7 @@ export default function Registro() {
                 </header>
 
                 {/* ── Formulario ── */}
-                <main className="flex-2 px-4 md:px-10 lg:px-30 md:my-auto py-10 lg:overflow-scroll">
+                <main className="flex-2 px-4 md:px-10 lg:px-30 md:my-auto py-10 lg:overflow-y-scroll">
                     <RegistroForm />
                 </main>
 
