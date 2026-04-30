3db6adeca1430d5305f67782c50dfc06a849dab3
Juan Alejandro Salgado Arcila <142946728+Vor7ex@users.noreply.github.com>
2026-04-30 12:15:15 -0500

    Change main tag to div in layout component

--- biblioteca-alejandria/app/(with-navbar)/layout.tsx
+++ biblioteca-alejandria/app/(with-navbar)/layout.tsx
@@ -8,7 +8,7 @@ export default function NavbarLayout({
   return (
     <>
       <Navbar />
-      <main className="flex-1 flex flex-col">{children}</main>
+      <div className="flex-1 flex flex-col">{children}</div>
     </>
   );
 }
